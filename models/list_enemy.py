01c6f5290097cf11828d866e324fba6ee71d1f43
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-07-23 06:35:16 +0800

    Add medium and hard enemy lists

--- models/list_enemy.py
+++ models/list_enemy.py
@@ -1,4 +1,7 @@
 from models.enemy import Enemy
 
 list_enemy_normal = ["goblin", "slime", "giant rat", "skeleton warrior", "kobold", "giant spider", "feral wolf", "imps"]
-list_enemy_medium = 
+
+list_enemy_medium = ["orc  berserker", "gargoyle", "lizardman", "harpy", "centaur archer", "zombie", "werewolf",  "basilisk"]
+
+list_enemy_hard = [""]
