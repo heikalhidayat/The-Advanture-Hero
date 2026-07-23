819b4333eee9cc0cb19dab3a07f7dae949f99fe3
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-07-23 17:44:04 +0800

    Add skills configuration for physical and magic attacks

---
+++ models/skills.py
@@ -0,0 +1,72 @@
+SKILLS = {
+      "PHYSICAL_ATTACK": {
+            "UNARMED": {
+                  "MELEE": {
+                        "basic_jab": {
+                              "damage": 10,
+                              "agility": 10
+                        },
+                        "low_kick": {
+                              "damage": 12,
+                              "agility": 15
+                        },
+                        "heavy_fist": {
+                              "damage": 15,
+                              "agility": 20
+                        }
+                  },
+                  "MID_RANGE": {
+                        "sweeping_leg": {
+                              "damage": 15,
+                              "agility": 20
+                        },
+                        "palm_push": {
+                              "damage": 10,
+                              "agility": 10,
+                              "energy": 15
+                        },
+                        "elbow_charge": {
+                              "damage": 15,
+                              "defense": 15,
+                              "agility": 10
+                        }
+                  },
+                  "LONG_RANGE": {
+                        "stone_throw": {
+                              "damage": 10,
+                              "agility": 12
+                        },
+                        "air_slap": {
+                              "energy": 20,
+                              "agility": 15
+                        }
+                  }
+            },
+            "ARMED": {
+                  "MELEE": {
+                  },
+                  "MID_RANGE": {
+                  },
+                  "LONG_RANGE": {
+                  }
+            }
+      },
+      "MAGIC_ATTACK": {
+            "UNARMED": {
+                  "MELEE": {
+                  },
+                  "MID_RANGE": {
+                  },
+                  "LONG_RANGE": {
+                  }
+            },
+            "ARMED": {
+                  "MELEE": {
+                  },
+                  "MID_RANGE": {
+                  },
+                  "LONG_RANGE": {
+                  }
+            }
+      }
+}
