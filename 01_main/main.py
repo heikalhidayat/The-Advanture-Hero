f9fa5554ee3b46b1f3bfcc65fbb872c6e4643ce6
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-07-29 11:36:27 +0800

    Enhance login and menu functionality with error handling
    Refactor login and menu functions, add error handling for menu choice.

--- 01_main/main.py
+++ 01_main/main.py
@@ -5,7 +5,7 @@ import time
 import random
 
 # Import constanta
-from config.config import DATABASE_NAME
+from config.config import DATABASE_NAME, MENU_OPTIONS
 from utils.database import init_database
 
 def login():
@@ -20,16 +20,17 @@ def login():
 
     if data_player is not None:
         id_player = data_player[0]
-        print(f"Welcome back {user_name}")
+        print(f"Welcome back {user_name} (ID: {id_player})")
 
-        # Load inventori dan wallet
+        # Load inventori
         cursor.execute("SELECT item_name FROM inventory WHERE id_player = ?", (id_player,))
         items = [item[0] for item in cursor.fetchall()]
 
+        # Load gold player
         cursor.execute("SELECT gold_player FROM wallet WHERE id_player = ?", (id_player,))
         gold_player = cursor.fetchone()[0]
-        print(f"Inventory: {items}")
-        print(f"Gold: {gold_player}")
+
+        print(f"\nGold: {gold_player}")
 
     else:
         cursor.execute("INSERT INTO username (user_name) VALUES (?)", (user_name,))
@@ -43,14 +44,48 @@ def login():
 
         print(f"\n[LOADING] WELCOME, {user_name} (ID: {id_player})")
         time.sleep(1)
+    
+    conn.close()
+
+    return id_player, user_name, items, gold_player
 
 def menu():
-    print("-" * 40, "\n   Welcome In Game The Advanture Hero   \n", "-" * 40, sep="")
+    print("\n------- MENU UTAMA -------")
     print("1. Main")
     print("2. Status karakter")
     print("3. Shop")
     print("4. Exit")
 
-init_database()
-login()
-menu()
+class InvalidMenuChoiceError(Exception):
+    pass
+
+def get_menu_choice():
+    while True:
+        try:
+            choice = int(input("choice menu: "))
+
+            if choice not in MENU_OPTIONS:
+                raise InvalidMenuChoiceError
+
+            return choice
+
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+        except InvalidMenuChoiceError as e:
+            print(f"X {e}")
+
+def main():
+    print("-" * 40, "\n   Welcome In Game The Advanture Hero   \n", "-" * 40, sep="")
+
+    while True:
+        init_database()
+        id_player, user_name, items, gold_player = login()
+        menu()
+        choice = get_menu_choice()
+
+# =================================================
+# MAIN PROGRAM
+# =================================================
+if __name__ == "__main__":
+    main()
