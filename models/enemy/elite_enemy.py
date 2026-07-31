0c7d6ab5ee8e149edf20b4f1f76195979be4c20a
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-07-31 11:09:52 +0800

    Add elite enemy classes with attributes and methods

---
+++ models/enemy/elite_enemy.py
@@ -0,0 +1,190 @@
+class Enemy:
+    '''Base class untuk semua monster'''
+    def __init__(
+        self, 
+        # Indentitas & progresi
+        name: str,
+        monster_type: str,
+        level: int = 0,
+        
+        # Pools
+        hp: int = 100, 
+        energy: int = 100,
+
+        # Core Stats 
+        strength: int = 10, 
+        agility: int = 10, 
+        defense=10,
+
+        # Item drop
+        drop_exp=10,
+        drop_item=None,
+    ):
+        self.name = name
+        self.monster_type = monster_type
+        self.level = level
+        self.hp = hp
+        self.energy = energy
+        self.strength = strength
+        self.agility = agility
+        self.defense = defense
+        self.drop_exp = drop_exp
+        self.drop_item = drop_item
+
+    def __repr__(self):
+        return f"<Karakter {self.name} L{self.level} HP:{self.hp}/{self.max_hp}>"
+
+    def take_damage(self, strength):
+      sisa_damage = strength - self.defense
+
+      if sisa_damage > 0:
+          self.hp -= sisa_damage
+          return True
+      else:
+          return False
+
+    def attack(self, target):
+        print(f"{self.name} menyerang {target.name} dengan damage {self.strength}")
+        terluka = target.take_damage(self.strength)
+        if terluka:
+            print(f"{target.name} terluka dengan damage {self.strength}")
+        else:
+            print(f"Serangan kurang efektif!! Defense {target.name} sangat tinggi!!")
+
+    def use_energy(self, amount: int) -> bool:
+        """Kurangi energy jika cukup, kembalikan True; jika tidak cukup, jangan ubah energy dan kembalikan False."""
+        if amount <= 0:
+            return True
+        if amount > self.energy:
+            print(f"{self.name} tidak memiliki energy yang cukup!")
+            return False
+        self.energy -= amount
+        return True
+
+class Minotaur(Enemy):
+    '''Monster berkepala banteng yang menjaga labirin dan koridor sempit'''
+    def __init__(self):
+        super().__init__(
+            name="Minotaur",
+            monster_type="elite",
+            level=1,
+            hp=400,
+            energy=350,
+            strength=600,
+            agility=345,
+            defense=450,
+            drop_exp=500,
+            drop_item=None,
+        )
+
+class Golem(Enemy):
+    '''Raksasa batu atau besi dengan pertahanan fisik absolut'''
+    def __init__(self):
+        super().__init__(
+            name="Golem",
+            monster_type="elite",
+            level=1,
+            hp=400,
+            energy=350,
+            strength=600,
+            agility=345,
+            defense=450,
+            drop_exp=500,
+            drop_item=None,
+        )
+
+class Wyvern(Enemy):
+    '''Kerabat naga yang lebih kecil namun sangat agresif di udara'''
+    def __init__(self):
+        super().__init__(
+            name="Wyvern",
+            monster_type="elite",
+            level=1,
+            hp=400,
+            energy=350,
+            strength=600,
+            agility=345,
+            defense=450,
+            drop_exp=500,
+            drop_item=None,
+        )
+
+class Necromancer(Enemy):
+    '''Penyihir hitam yang terus menerus membangkitkan monster mati'''
+    def __init__(self):
+        super().__init__(
+            name="Necromancer",
+            monster_type="elite",
+            level=1,
+            hp=400,
+            energy=350,
+            strength=600,
+            agility=345,
+            defense=450,
+            drop_exp=500,
+            drop_item=None,
+        )
+
+class Cyclops(Enemy):
+    '''Raksasa bermata satu yang mampu melempar batu besar'''
+    def __init__(self):
+        super().__init__(
+            name="Cyclops",
+            monster_type="elite",
+            level=1,
+            hp=400,
+            energy=350,
+            strength=600,
+            agility=345,
+            defense=450,
+            drop_exp=500,
+            drop_item=None,
+        )
+
+class Succubus(Enemy):
+    '''Monster penggoda yang mencuri nyawa (HP) menggunkan sihir'''
+    def __init__(self):
+        super().__init__(
+            name="Succubus",
+            monster_type="elite",
+            level=1,
+            hp=400,
+            energy=350,
+            strength=600,
+            agility=345,
+            defense=450,
+            drop_exp=500,
+            drop_item=None,
+        )
+
+class Chimera(Enemy):
+    '''Makhluk hibrida berkepala singa, kambing, dan berekor ular'''
+    def __init__(self):
+        super().__init__(
+            name="Chimera",
+            monster_type="elite",
+            level=1,
+            hp=400,
+            energy=350,
+            strength=600,
+            agility=345,
+            defense=450,
+            drop_exp=500,
+            drop_item=None,
+        )
+
+class Lich(Enemy):
+    '''Penyihir mayat hidup yang menguasai sihir es tingkat tinggi'''
+    def __init__(self):
+        super().__init__(
+            name="Lich",
+            monster_type="elite",
+            level=1,
+            hp=400,
+            energy=350,
+            strength=600,
+            agility=345,
+            defense=450,
+            drop_exp=500,
+            drop_item=None,
+        )
