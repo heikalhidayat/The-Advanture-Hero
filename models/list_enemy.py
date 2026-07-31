61befdccec9500b4252d104cdd76ffe1073cfe47
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-07-31 15:17:56 +0800

    Add new enemy categories to list_enemy.py
    Added new enemy lists for rare, elite, legendary, and mythic categories.

--- models/list_enemy.py
+++ models/list_enemy.py
@@ -1,7 +1,9 @@
-from models.enemy import Enemy
-
 list_enemy_normal = ["goblin", "slime", "giant rat", "skeleton warrior", "kobold", "giant spider", "feral wolf", "imps"]
 
-list_enemy_medium = ["orc  berserker", "gargoyle", "lizardman", "harpy", "centaur archer", "zombie", "werewolf",  "basilisk"]
+list_enemy_rare = ["orc  berserker", "gargoyle", "lizardman", "harpy", "centaur archer", "zombie", "werewolf",  "basilisk"]
 
-list_enemy_hard = [""]
+list_enemy_elite = ["minotaur", "golem", "wyvern", "necromancer", "cyclops", "succubus", "chimera", "lich"]
+
+list_enemy_legendary = ["ancient dragon", "kraken", "phoenix", "leviathan", "titan"]
+
+list_enemy_mythic = ["cthulhu mythos", "bahamut"]
