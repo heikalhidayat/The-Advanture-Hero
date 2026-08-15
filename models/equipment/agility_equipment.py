9f667ac29827041ff03d6b9ecff732016c7c77e4
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-08-15 15:48:57 +0800

    Add Agility class for agility equipment

---
+++ models/equipment/agility_equipment.py
@@ -0,0 +1,16 @@
+class Agility(Equipment):
+    def __init__(
+        self,
+        name: str,
+        category: "Agility",
+        kind: str,
+        capasity: int,
+        base_durability: int,
+        agility: int
+    ):
+        super().__init__(name, category, kind, capasity, base_durability)
+        self.agility = agility
+
+    def total_agility(self, amount: int) -> int:
+        self.agility += amount
+        return self.agility
