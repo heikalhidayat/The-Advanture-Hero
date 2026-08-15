2d228fe6233e879943c807aa5ceebc1dd05d76a1
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-08-15 15:34:02 +0800

    Add Strength class for strength equipment

---
+++ models/equipment/strength_equipment.py
@@ -0,0 +1,16 @@
+class Strength(Equipment):
+    def __init__(
+        self,
+        name: str,
+        category: "Strength",
+        kind: str,
+        capasity: int,
+        base_durability: int,
+        strength: int
+    ):
+        super().__init__(name, category, kind, capasity, base_durability)
+        self.strength = strength
+
+    def total_strength(self, amount: int) -> int:
+        self.strength += amount
+        return self.strength
