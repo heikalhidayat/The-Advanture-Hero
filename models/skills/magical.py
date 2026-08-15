f17c8cf5810ae3fc781b7d790ae9c5017768ca57
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-08-15 11:25:56 +0800

    Refactor magical skills class constructors
    Refactor magical skills to inherit from Skills class and update constructors.

--- models/skills/magical.py
+++ models/skills/magical.py
@@ -1,89 +1,193 @@
-class Skills:
-    def __init__(self, name, category, target_area, range_type, debuff, mana, damage):
-        self.name = name
-        self.category = "magical_attack"
-        self.target_area = target_area
-        self.range_type = range_type
-        self.debuff = debuff
-        self.mana = mana
-        self.damage = damage
-        
-    def execute(self, user, target):
-        if user.mana >= self.mana:
-            user.mana -= self.mana
-            print(f"{user.name} menggunakan {self.name}!")
-            target.take_damage(self.damage)
-        else:
-            print(f"Mana {user.name} tidak cukup untuk {self.name}!")
+from base_skills import Skills
+
+from config import (
+      BASE_ENERGY,
+      BASE_MANA,
+      BASE_MAGIC,
+      BASE_RESISTANCE,
+      BASE_INTELLIGENCE
+)
 
 class ManaBurst(Skills):
-    '''Ledakan energi instan di telapak tangan'''
-    def __init__(self, category):
-        super().__init__(
-              name="Mana Burst",
-              target_area="single_target",
-              range_type="melee",
-              debuff="knockback",
-              mana=15,
-              damage=35
-        )
+    def __init__(
+        self,
+        name = "Mana Burst",
+        category = "Magical",
+        armed = False,
+        range_type = "Ranged",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = BASE_MAGIC,
+        dexterity = 0,
+        resistance = BASE_RESISTANCE,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(name, category, armed, range_type, debuff, level, competence, energy, mana, strength, agility, defense, vitality, magic, dexterity, resistance, intelligence)
 
 class ShockTounch(Skills):
-    '''sentuhan listrik bertegangan tinggi'''
-    def __init__(self, category):
-        super().__init__(
-              name="Shock Tounch",
-              target_area="Single Target",
-              range_type="melee",
-              debuff="stun",
-              mana=20,
-              damage=25
-        )
+    def __init__(
+        self,
+        name = "Shock Tounch",
+        category = "Magical",
+        armed = False,
+        range_type = "Ranged",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = BASE_MAGIC,
+        dexterity = 0,
+        resistance = BASE_RESISTANCE,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(name, category, armed, range_type, debuff, level, competence, energy, mana, strength, agility, defense, vitality, magic, dexterity, resistance, intelligence)
+
+class ShockWave(Skills):
+    def __init__(
+        self,
+        name = "Shock Wave",
+        category = "Magical",
+        armed = False,
+        range_type = "Ranged",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = BASE_MAGIC,
+        dexterity = 0,
+        resistance = BASE_RESISTANCE,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(name, category, armed, range_type, debuff, level, competence, energy, mana, strength, agility, defense, vitality, magic, dexterity, resistance, intelligence)
 
 class RepelWave(Skills):
-    '''gelombang dorongan sihir berbentuk kipas'''
-    def __init__(self, category):
-        super().__init__(
-              name="Repel Wave",
-              target_area="Area of Effect",
-              range_type="mid_range",
-              debuff="slow",
-              mana=25,
-              damage=40
-        )
+    def __init__(
+        self,
+        name = "Repel Wave",
+        category = "Magical",
+        armed = False,
+        range_type = "Ranged",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = BASE_MAGIC,
+        dexterity = 0,
+        resistance = BASE_RESISTANCE,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(name, category, armed, range_type, debuff, level, competence, energy, mana, strength, agility, defense, vitality, magic, dexterity, resistance, intelligence)
 
 class ArcaneRing(Skills):
-    '''Lingkaran energy yang meledak di sekeliling tubuh'''
-    def __init__(self, category):
-        super().__init__(
-              name="Arcane Ring",
-              target_area="Area of Effect",
-              range_type="mid_range",
-              debuff=None,
-              mana=30,
-              damage=50
-        )
+    def __init__(
+        self,
+        name = "Arcane Ring",
+        category = "Magical",
+        armed = False,
+        range_type = "Ranged",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = BASE_MAGIC,
+        dexterity = 0,
+        resistance = BASE_RESISTANCE,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(name, category, armed, range_type, debuff, level, competence, energy, mana, strength, agility, defense, vitality, magic, dexterity, resistance, intelligence)
+
+class MagicShield(Skills):
+    def __init__(
+        self,
+        name = "Magic Shield",
+        category = "Magical",
+        armed = False,
+        range_type = "Ranged",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = BASE_MAGIC,
+        dexterity = 0,
+        resistance = BASE_RESISTANCE,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(name, category, armed, range_type, debuff, level, competence, energy, mana, strength, agility, defense, vitality, magic, dexterity, resistance, intelligence)
 
 class MagicArrow(Skills):
-    '''Proyektil sihir standar berbentuk panah cahaya'''
-    def __init__(self, category):
-        super().__init__(
-              name="Magic Arrow",
-              target_area="Single Target",
-              range_type="long_range",
-              debuff=None,
-              mana=10,
-              damage=30
-        )
+    def __init__(
+        self,
+        name = "Magic Arrow",
+        category = "Magical",
+        armed = False,
+        range_type = "Ranged",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = BASE_MAGIC,
+        dexterity = 0,
+        resistance = BASE_RESISTANCE,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(name, category, armed, range_type, debuff, level, competence, energy, mana, strength, agility, defense, vitality, magic, dexterity, resistance, intelligence)
 
 class SparkProjectile(Skills):
-    '''Bola listrik kecil yang dilempar dari jauh'''
-    def __init__(self, category):
-        super().__init__(
-              name="Spark Projectile",
-              target_area="Single Target",
-              range_type="long_range",
-              debuff="paralyze",
-              mana=15,
-              damage=25
-        )
+    def __init__(
+        self,
+        name = "Spark Projectile",
+        category = "Magical",
+        armed = False,
+        range_type = "Ranged",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = BASE_MAGIC,
+        dexterity = 0,
+        resistance = BASE_RESISTANCE,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(name, category, armed, range_type, debuff, level, competence, energy, mana, strength, agility, defense, vitality, magic, dexterity, resistance, intelligence)
