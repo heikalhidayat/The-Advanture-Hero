ca17b5522448112d3dcd4537933e17ba45438059
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-08-15 14:18:04 +0800

    Refactor equipment class to include kind and durability

--- models/equipment/equipment.py
+++ models/equipment/equipment.py
@@ -3,10 +3,38 @@ class Equipment:
         self,
         name: str,
         category: str,
-        level: int,
-        durability: int,
+        kind: str,
+        base_durability: int,
     ):
         self.name = name
         self.category = category
-        self.level = level
-        self.durability = durability
+        self.kind = kind
+        self.base_durability = base_durability
+        self.current_durability = base_durability
+    
+    def repair(self, amount: int):
+        self.current_durability += amount
+        if self.current_durability > self.base_durability:
+            self.current_durability = self.base_durability
+    
+    def use(self, amount: int):
+        self.current_durability -= amount
+        if self.current_durability < 0:
+            self.current_durability = 0
+
+    def __repr__(self):
+        try:
+            dur_max = self.base_durability
+        except Exception:
+            dur_max = "?"
+        return f"{self.name!r} ({self.category!r}, {self.kind!r}, {self.current_durability}/{dur_max})"
+
+    def __str__(self) -> str:
+        return (
+            "====================\n"
+            f"< {self.name.upper()} >\n"
+            "====================\n"
+            f"[{self.current_durability}/{self.base_durability}]\n\n"
+            f"Category: {self.category}\n"
+            f"Kind: {self.kind}"
+        )
