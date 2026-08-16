853d83dc64134b22398c162e2bc2fe752a04f5dc
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-08-16 11:55:16 +0800

    Add agility equipment classes for game mechanics

--- models/equipment/agility_equipment.py
+++ models/equipment/agility_equipment.py
@@ -15,3 +15,85 @@ class Agility(Equipment):
     def total_agility(self, amount: int) -> int:
         self.agility += amount
         return self.agility
+
+class Dagger(Agility):
+    '''
+    Pisau genggam ringan, memungkinkan penggunanya melakukan tusukan
+    cepat bertubi-tubi.
+    '''
+    def __init__(
+        self,
+        name = "Dagger",
+        category = "Agility",
+        kind = "Weapon",
+        price = 100,
+        capasity = 2,
+        base_durability = BASE_DURABILITY,
+        agility = 4
+    ):
+        super().__init__(name, category, kind, price, capasity, base_durability, agility)
+
+class LeatherBoots(Agility):
+    '''
+    Sepatu berbahan kulit elastis yang tidak bersuara, 
+    dirancang khusus untuk meningkatkan kecepatan.
+    '''
+    def __init__(
+        self,
+        name = "Leather Boots",
+        category = "Agility",
+        kind = "Armor",
+        price = 80,
+        capasity = 2,
+        base_durability = BASE_DURABILITY,
+        agility = 3
+    ):
+        super().__init__(name, category, kind, price, capasity, base_durability, agility)
+
+class ClothCloak(Agility):
+    '''
+    Jubah kain tipis yang berkibar mengikuti arah angin.
+    '''
+    def __init__(
+        self,
+        name = "Cloth Cloak",
+        category = "Agility",
+        kind = "Armor",
+        price = 150,
+        capasity = 4,
+        base_durability = BASE_DURABILITY,
+        agility = 5
+    ):
+        super().__init__(name, category, kind, price, capasity, base_durability, agility)
+
+class SilverNecklace(Agility):
+    '''
+    Kalung perak yang memberikan erek meringankan beban tubuh.
+    '''
+    def __init__(
+        self,
+        name = "Silver Necklace",
+        category = "Agility",
+        kind = "Accessory",
+        price = 200,
+        capasity = 1,
+        base_durability = BASE_DURABILITY,
+        agility = 4
+    ):
+        super().__init__(name, category, kind, price, capasity, base_durability, agility)
+
+class PocketWatch(Agility):
+    '''
+    Jam saku kuno yang bisa memperlambat waktu.
+    '''
+    def __init__(
+        self,
+        name = "Pocket Watch",
+        category = "Agility",
+        kind = "Accessory",
+        price = 300,
+        capasity = 1,
+        base_durability = BASE_DURABILITY,
+        agility = 8
+    ):
+        super().__init__(name, category, kind, price, capasity, base_durability, agility)
