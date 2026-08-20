17ca1fde69f7fcfe4cd7d5467b5b6f2915aead2b
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-08-20 17:16:29 +0800

    Add ShockTouch class for melee magical skill

---
+++ models/skills/magical/melee.py
@@ -0,0 +1,36 @@
+from base_skills import Skills
+
+from config import (
+      BASE_ENERGY,
+      BASE_MANA,
+      BASE_MAGIC,
+      BASE_RESISTANCE,
+      BASE_INTELLIGENCE
+)
+
+class ShockTounch(Skills):
+    def __init__(
+        self,
+        name = "Shock Tounch",
+        category = "Magical",
+        armed = False,
+        range_type = "Melee",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = 5,
+        dexterity = 4,
+        resistance = 0,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(
+            name, category, armed, range_type, debuff, level, competence, 
+            energy, mana, strength, agility, defense, vitality, magic, dexterity,
+            resistance, intelligence
+        )
