b9b032bd1dc05e51ffd64f1b13a3b02397895d49
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-08-24 13:32:14 +0800

    Refactor Enemy class constructor to use type hints

--- models/enemy/base_enemy.py
+++ models/enemy/base_enemy.py
@@ -4,40 +4,47 @@ class Enemy(Karakter):
     def __init__(
         self,
         name,
-        job = None,
-        tier = 1,
-        level = 1,
-        exp = 0,
-        skill_01 = None,
-        skill_02 = None,
-        skill_03 = None,
-        skill_04 = None,
-        base_hp = 0,
-        base_energy = 0,
-        base_mana = 0,
-        strength = 0,
-        agility = 0,
-        defense = 0,
-        vitality = 0,
-        magic = 0,
-        dexterity = 0,
-        resistance = 0,
-        intelligence = 0,
-        strength_bonus = 0,
-        agility_bonus = 0,
-        defense_bonus = 0,
-        magic_bonus = 0,
-        dexterity_bonus = 0,
-        resistance_bonus = 0
+        job: str,
+        tier: int,
+        level: int,
+        exp: int,
+        skill_01: str,
+        skill_02: int,
+        skill_03: int,
+        skill_04: int,
+        # base values
+        base_hp: int,
+        base_energy: int,
+        base_mana: int,
+        # core stats
+        strength: int,
+        agility: int,
+        defense: int,
+        vitality: int,
+        magic: int,
+        dexterity: int,
+        resistance: int,
+        intelligence: int,
+        # equipment bonus
+        strength_bonus: int,
+        agility_bonus: int,
+        defense_bonus: int,
+        magic_bonus: int,
+        dexterity_bonus: int,
+        resistance_bonus: int,
+        # drop
+        drop_item: str,
+        drop_exp: int,
     ):
         super().__init__(
-            name, job, tier, level, exp, skill_01, skill_02, skill_03, skill_04, base_hp, base_energy,
-            base_mana, strength, agility, defense, vitality, magic,
-            dexterity, resistance, intelligence, strength_bonus, 
+            name, job, tier, level, exp, skill_01, skill_02, skill_03, skill_04,
+            base_hp, base_energy, base_mana, strength, agility, defense, vitality, 
+            magic, dexterity, resistance, intelligence, strength_bonus, 
             agility_bonus, defense_bonus, magic_bonus, dexterity_bonus, 
             resistance_bonus
         )
-        self.drop_item = None
+        self.drop_item = drop_item
+        self.drop_exp = drop_exp
 
     @property
     def total_drop_exp(self) -> int:
