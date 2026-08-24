753911d83c86701a429b15ddf7123a683711bf96
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-08-24 16:52:28 +0800

    Add SlimeDivine class to melee magical skills

--- models/skills/magical/melee_magical.py
+++ models/skills/magical/melee_magical.py
@@ -34,3 +34,30 @@ class ShockTounch(Skills):
             energy, mana, strength, agility, defense, vitality, magic, dexterity,
             resistance, intelligence
         )
+
+class SlimeDivine(Skills):
+    def __init__(
+        self,
+        name = "Slime Divine",
+        category = "Magical",
+        armed = False,
+        range_type = "Melee",
+        debuff = "Stun",
+        level = 1,
+        competence = 0,
+        energy = BASE_ENERGY,
+        mana = BASE_MANA,
+        strength = 0,
+        agility = 0,
+        defense = 0,
+        vitality = 0,
+        magic = 5,
+        dexterity = 4,
+        resistance = 0,
+        intelligence = BASE_INTELLIGENCE
+    ):
+        super().__init__(
+            name, category, armed, range_type, debuff, level, competence, 
+            energy, mana, strength, agility, defense, vitality, magic, dexterity,
+            resistance, intelligence
+        )
