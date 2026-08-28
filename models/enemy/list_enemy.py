d11ec426f7782c2747d7e7c362d28a5b5b9a88f4
Heikal Hidayat <heikalhidayat03@gmail.com>
2026-08-28 17:18:20 +0800

    Refactor enemy lists with updated names and casing

--- models/enemy/list_enemy.py
+++ models/enemy/list_enemy.py
@@ -1,9 +1,9 @@
-list_enemy_normal = ["goblin", "slime", "giant rat", "skeleton warrior", "kobold", "giant spider", "feral wolf", "imps"]
+list_enemy_normal = ["Slime", "GoblinScout", "GiantRat", "GoblinWarrior", "SkeletonWarrior", "Kobold", "GiantSpider", "FeralWolf", "Imps"]
 
-list_enemy_rare = ["orc  berserker", "gargoyle", "lizardman", "harpy", "centaur archer", "zombie", "werewolf",  "basilisk"]
+list_enemy_rare = ["OrcBerseker", "Gargoyle", "Lizardman", "Harpy", "CentaurArcher", "ZombiePlaguebringer", "Werewolf", "Basilisk"]
 
-list_enemy_elite = ["minotaur", "golem", "wyvern", "necromancer", "cyclops", "succubus", "chimera", "lich"]
+list_enemy_elite = ["Minotaur", "Golem", "Wyvern", "Cyclops", "Succubus", "Chimera", "Lich"]
 
-list_enemy_legendary = ["ancient dragon", "kraken", "phoenix", "leviathan", "titan"]
+list_enemy_legendary = ["AncientDragon", "Kraken", "Phoenix", "Leviathan", "Titan"]
 
 list_enemy_mythic = ["cthulhu mythos", "bahamut"]
