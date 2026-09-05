7bd8f2d1ba192bc111c083b3a7734218cb57d5a5
heikalhidayat <heikalhidayat03@gmail.com>
2026-09-05 15:38:02 +0800

    update api.py

--- api.py
+++ api.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Server is running!"
 
+@app.route("/karakter/<id>")
+def get_karakter(id):
+    return f"Fetching karakter with ID: {id}"
+
 if __name__ == "__main__":
     app.run(debug=True)
