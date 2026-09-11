5873ad6ec024868db40547664d55752d610367e6
heikalhidayat <heikalhidayat03@gmail.com>
2026-09-11 09:09:45 +0800

    update API

--- api.py
+++ api.py
@@ -1,10 +1,6 @@
 from flask import (Flask, jsonify, request)
 
-from utils.database import (
-    get_username_by_id, get_inventory_by_id, get_pocket_by_id,
-    get_karakter_by_id, get_skills_by_id, get_equipment_by_id,
-    get_by_id
-)
+from utils.database import get_by_id
 
 app = Flask(__name__)
 
@@ -19,27 +15,27 @@ def get_user(id):
 
 @app.route("/inventory/<id>")
 def get_inventory(id):
-    data = get_inventory_by_id(id)
+    data = get_by_id("invetory", "id_item", id)
     return jsonify(data)
 
 @app.route("/pocket/<id>")
 def get_pocket(id):
-    data = get_pocket_by_id(id)
+    data = get_by_id("pocket", "id_pocket", id)
     return jsonify(data)
 
 @app.route("/karakter/<id>")
 def get_karakter(id):
-    data = get_karakter_by_id(id)
+    data = get_by_id("karakter", "id_karakter", id)
     return jsonify(data)
 
 @app.route("/skills/<id>")
 def get_skills(id):
-    data = get_skills_by_id(id)
+    data = get_by_id("skills", "id_skills", id)
     return jsonify(data)
 
 @app.route("/equipment/<id>")
 def get_equipment(id):
-    data = get_equipment_by_id(id)
+    data = get_by_id("equipment", "id_equipment", id)
     return jsonify(data)
 
 if __name__ == "__main__":
